513d9165b579c09e5d1d97ceac9a46f0e80f8990
Luke Twomey <luke.twomey@asos.com>
2022-02-25 15:46:39 +0000

    Restructure files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/pro-regular-svg-icons";
+
+function App() {
+  return (
+    <div className="App">
+      <FontAwesomeIcon icon={faEdit} className="edit" />
+    </div>
+  );
+}
+
+export default App;

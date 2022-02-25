cc8a083684221928bba2a59f986bb26c1b8d8a49
Luke Twomey <luke.twomey@asos.com>
2022-02-25 17:41:03 +0000

    Add useEffect hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit } from "@fortawesome/pro-regular-svg-icons";
 
 function App() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
 
   return (
     <div>

42e7a762fbb36a0acbed6d60bd7332ecb8033d55
Luke Twomey <luke.twomey@asos.com>
2022-02-25 17:31:38 +0000

    Add useState hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit } from "@fortawesome/pro-regular-svg-icons";
 
 function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
       <FontAwesomeIcon icon={faEdit} className="edit" />
     </div>
   );

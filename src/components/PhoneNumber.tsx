11919ad0d8e428f5389cc089a3286078f231086d
Luke Twomey <luke.twomey85@gmail.com>
2024-01-22 10:43:34 +0000

    remove the prevent default

--- src/components/PhoneNumber.tsx
+++ src/components/PhoneNumber.tsx
@@ -3,13 +3,9 @@ import styles from "./PhoneNumber.module.css";
 import { FaPhoneAlt } from "react-icons/fa";
 
 const PhoneNumber = ({ number }) => {
-  const onLinkClick = (e) => {
-    e.preventDefault();
-  };
-
   return (
     <div className={styles.phone}>
-      <a href={`tel:${number}`} className={styles.number} onClick={onLinkClick}>
+      <a href={`tel:${number}`} className={styles.number}>
         <div className={styles.phoneIcon}>
           <FaPhoneAlt />
         </div>{" "}

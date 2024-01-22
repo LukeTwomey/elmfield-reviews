d32d16c2093f1678652c8b902476cdf9a53865da
Luke Twomey <luke.twomey85@gmail.com>
2024-01-22 11:34:13 +0000

    Add review dates

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import moment from "moment";
 import styles from "./Review.module.css";
 import ReviewStars from "./ReviewStars";
 
 const Review = ({ review }) => {
+  const date = moment(review.date).format("Do MMMM YYYY");
+
   return (
     <div className={styles.review}>
       <div className={styles.top}>
-        <div className={styles.author}>{review.author}</div>
+        <div className={styles.left}>
+          <div className={styles.author}>{review.author}</div>
+          <div className={styles.date}>{date}</div>
+        </div>
         <ReviewStars rating={review.rating} />
       </div>
       <div className={styles.content}>{`"${review.review}"`}</div>

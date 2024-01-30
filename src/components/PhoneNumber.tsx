c272c016e18d1211797b168b1475de721ff89076
Luke Twomey <luke.twomey85@gmail.com>
2024-01-30 17:40:43 +0000

    Hide phone number icon if no number

--- src/components/PhoneNumber.tsx
+++ src/components/PhoneNumber.tsx
@@ -3,7 +3,7 @@ import styles from "./PhoneNumber.module.css";
 import { FaPhoneAlt } from "react-icons/fa";
 
 const PhoneNumber = ({ number }) => {
-  return (
+  return number ? (
     <div className={styles.phone}>
       <a href={`tel:${number}`} className={styles.number}>
         <div className={styles.phoneIcon}>
@@ -12,7 +12,7 @@ const PhoneNumber = ({ number }) => {
         <div>{number}</div>
       </a>
     </div>
-  );
+  ) : null;
 };
 
 export default PhoneNumber;
